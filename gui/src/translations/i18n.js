ded6da76f657b028ba218f7139ba2e5cd5aad957
tomyste <tomyste02@gmail.com>
2025-07-10 18:53:10 -0300

    Modify the graphical user interface to automatically adapt new languages

--- gui/src/translations/i18n.js
+++ gui/src/translations/i18n.js
@@ -3,6 +3,9 @@ import { initReactI18next } from 'react-i18next'
 import global_es from './es/global.json'
 import global_en from './en/global.json'
 import global_fr from './fr/global.json'
+import global_de from './de/global.json' 
+import global_it from './it/global.json'
+import global_pt from './pt/global.json'
 
 i18n
   .use(initReactI18next)
@@ -21,7 +24,16 @@ i18n
       },
       fr: {
         translation: global_fr,
-      }
+      },
+      de: {
+        translation: global_de, 
+      },
+      it: {
+        translation: global_it, 
+      },
+      pt: {
+        translation: global_pt, 
+      },
     },
   })
 
